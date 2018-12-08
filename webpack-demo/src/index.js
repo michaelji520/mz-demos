e7cd54a3ff540b3a0fa1e2e380f2e393ca1ccc49
zhangji02 <zhangji02@zuoyebang.com>
2018-12-08 10:48:27 +0800

    [UPDATE] COMMIT 1

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,29 +1,12 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+function getComponent() {
+  return import(/* webpackChunkname: "lodash" */ 'lodash').then(({ default: _ }) => {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+    return element;
+  }).catch(error => 'An error occurred while loading the component');
 }
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
 
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component();
-    document.body.appendChild(element);
-  })
-}
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,32 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    app: './src/index.js',
+    index: './src/index.js',
   },
-  // devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  module: {
-    rules: [
-      {test: /\.css$/, use: ['style-loader', 'css-loader']}
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
   output: {
-    filename: '[name]-[hash:8].bundle.js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
   },
-}
+};
