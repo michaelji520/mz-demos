ca8d4d767b525ae1765b89627ca54bd05606d343
zhangji02 <zhangji02@zuoyebang.com>
2019-11-11 22:04:34 +0800

    init:rich-text

---
+++ html/rich-text/app.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2014-2018 NighthawkStudio, All rights reserved.
+ * @fileoverview
+ * @author Michael Zhang | michaelji520@gmail.com
+ * @version 1.0 | 2019-11-11 | initial version
+ */
+
+
